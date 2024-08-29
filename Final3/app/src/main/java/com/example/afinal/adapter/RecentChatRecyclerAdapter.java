675ab9ce7270936c7e899d0bf59b8d5d9c63ae8c
Amits232 <122890393+Amits232@users.noreply.github.com>
2024-08-29 15:53:05 +0300

    Update chats screen full

---
+++ Final3/app/src/main/java/com/example/afinal/adapter/RecentChatRecyclerAdapter.java
@@ -0,0 +1,94 @@
+package com.example.afinal.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.afinal.ChatActivity;
+import com.example.afinal.R;
+import com.example.afinal.model.ChatRoomModel;
+import com.example.afinal.model.UserModel;
+import com.example.afinal.utils.AndoridUtil;
+import com.example.afinal.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.auth.User;
+
+import org.w3c.dom.Text;
+
+public class RecentChatRecyclerAdapter extends FirestoreRecyclerAdapter<ChatRoomModel, RecentChatRecyclerAdapter.ChatroomModelViewHolder> {
+
+    Context context;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public RecentChatRecyclerAdapter(@NonNull FirestoreRecyclerOptions<ChatRoomModel> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onBindViewHolder(@NonNull ChatroomModelViewHolder holder, int position, @NonNull ChatRoomModel model) {
+        FirebaseUtil.getOtherUserFromChatroom(model.getUserIds())
+                .get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        boolean lastMessageSentByMe = model.getLastMessageSenderId().equals(FirebaseUtil.currentUserId());
+
+                        UserModel otherUserModel = task.getResult().toObject(UserModel.class);
+                        holder.usernameText.setText(otherUserModel.getUsername());
+                        if(lastMessageSentByMe)
+                            holder.lastMessageText.setText("את/ה : " + model.getLastMessage());
+                        else
+                            holder.lastMessageText.setText(model.getLastMessage());
+                        holder.lastMessageTime.setText(FirebaseUtil.timestampToString(model.getLastMessageTimestamp()));
+
+                        holder.itemView.setOnClickListener(v -> {
+                            Intent intent = new Intent(context, ChatActivity.class);
+                            AndoridUtil.passUserModelAsIntent(intent, otherUserModel);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+                        });
+                    }
+                });
+    }
+
+    @NonNull
+    @Override
+    public ChatroomModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recent_chat_recycler_row, parent, false);
+        return new ChatroomModelViewHolder(view);
+    }
+
+    class ChatroomModelViewHolder extends RecyclerView.ViewHolder {
+        TextView usernameText;
+        TextView lastMessageText;
+        TextView lastMessageTime;
+        ImageView profilePic;
+
+
+        public ChatroomModelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameText = itemView.findViewById(R.id.user_name_text);
+            lastMessageText = itemView.findViewById(R.id.last_message_text);
+            lastMessageTime = itemView.findViewById(R.id.last_message_time_text);
+            profilePic = itemView.findViewById(R.id.profile_pic_image_view);
+        }
+    }
+
+}
--- Final3/app/src/main/java/com/example/afinal/utils/FirebaseUtil.java
+++ Final3/app/src/main/java/com/example/afinal/utils/FirebaseUtil.java
@@ -1,9 +1,13 @@
 package com.example.afinal.utils;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class FirebaseUtil {
     public static String currentUserId(){
@@ -35,5 +39,22 @@ public class FirebaseUtil {
         }else {
             return uid2+"_"+uid1;
         }
+    }
+
+    public static CollectionReference allChatroomCollectionRefrence(){
+        return  FirebaseFirestore.getInstance().collection("chatrooms");
+    }
+
+    public static DocumentReference getOtherUserFromChatroom(List<String> userIds){
+        if(userIds.get(0).equals(FirebaseUtil.currentUserId())){
+            return allUserCollectionReference().document(userIds.get(1));
+        }
+        else{
+            return allUserCollectionReference().document(userIds.get(0));
+        }
+    }
+
+    public static  String timestampToString(Timestamp timestamp){
+        return new SimpleDateFormat("HH:MM").format(timestamp.toDate());
     }
 }
